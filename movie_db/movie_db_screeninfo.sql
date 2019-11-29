3da7c3fa68893864ec7f444b64e7e7fafdbe0b05
Sunghoon3772 <49020247+Sunghoon3772@users.noreply.github.com>
2019-11-30 00:44:31 +0900

    movie db

---
+++ movie_db/movie_db_screeninfo.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: movie_db
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `screeninfo`
+--
+
+DROP TABLE IF EXISTS `screeninfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `screeninfo` (
+  `ID` int(11) NOT NULL,
+  `start_date` date DEFAULT NULL,
+  `start_time` time DEFAULT NULL,
+  `Movie_ID` int(11) DEFAULT NULL,
+  `Branch_ID` int(11) DEFAULT NULL,
+  `Theater_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `Movie_ID` (`Movie_ID`),
+  KEY `Branch_ID` (`Branch_ID`,`Theater_ID`),
+  CONSTRAINT `screeninfo_ibfk_1` FOREIGN KEY (`Movie_ID`) REFERENCES `movie` (`ID`),
+  CONSTRAINT `screeninfo_ibfk_2` FOREIGN KEY (`Branch_ID`, `Theater_ID`) REFERENCES `theater` (`Branch_ID`, `ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `screeninfo`
+--
+
+LOCK TABLES `screeninfo` WRITE;
+/*!40000 ALTER TABLE `screeninfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `screeninfo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-30  0:37:29
